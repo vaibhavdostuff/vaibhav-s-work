e3d98ae86111f8a85f66db7182500ed535fe0898
vaibhavdostuff <vaibhavnegi141@gmail.com>
2023-12-28 23:58:40 +0530

    v pattern code

--- .py
+++ .py
@@ -1,1 +1,12 @@
-print("hello world") 
+row_size = int(input("Enter the row size:"))
+print_control_x = 1
+print_control_y = row_size*2-1
+for out in range(1, row_size+1):
+    for inn in range(1, row_size*2+1):
+        if inn == print_control_x or inn == print_control_y:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print_control_x += 1
+    print_control_y -= 1
+    print("\r")
