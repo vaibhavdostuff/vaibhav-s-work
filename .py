618e71255c196b9fc269328357f86a5d29ce2aca
@Vaibhav Negi <148843245+vaibhavdostuff@users.noreply.github.com>
2023-12-31 23:59:52 +0530

    Update .py

--- .py
+++ .py
@@ -2,7 +2,7 @@ row_size = int(input("Enter the row size:"))
 print_control_x = 1
 print_control_y = row_size*2-1
 for out in range(1, row_size+1):
-    for inn in range(1, row_size*2+1):
+    for    inn in range(1, row_size*2+1):
         if inn == print_control_x or inn == print_control_y:
             print("*", end="")
         else:
