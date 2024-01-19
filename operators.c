994909f1afd89724b7351f9e15c247f19d1e1abf
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-19 23:23:10 +0530

    c operators programm

---
+++ operators.c
@@ -0,0 +1,22 @@
+//1
+// C program to illustrate the arithmatic operators 
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int a = 25, b = 5; 
+
+	// using operators and printing results 
+	printf("a + b = %d\n", a + b); 
+	printf("a - b = %d\n", a - b); 
+	printf("a * b = %d\n", a * b); 
+	printf("a / b = %d\n", a / b); 
+	printf("a % b = %d\n", a % b); 
+	printf("+a = %d\n", +a); 
+	printf("-a = %d\n", -a); 
+	printf("a++ = %d\n", a++); 
+	printf("a-- = %d\n", a--); 
+
+	return 0; 
+}
