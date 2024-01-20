c662e0013fe19ea070a784993bfa9ebbae121e27
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-20 20:15:52 +0530

    c operators programm

--- operators.c
+++ operators.c
@@ -43,3 +43,22 @@ int main()
 
 
 //3
+// C program to illustrate the bitwise operators 
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int a = 25, b = 5; 
+
+	// using operators and printing results 
+	printf("a & b: %d\n", a & b); 
+	printf("a | b: %d\n", a | b); 
+	printf("a ^ b: %d\n", a ^ b); 
+	printf("~a: %d\n", ~a); 
+	printf("a >> b: %d\n", a >> b); 
+	printf("a << b: %d\n", a << b); 
+
+	return 0; 
+}
+
