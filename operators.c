d024925783f7b42ad028980151df90393ca12677
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-20 20:14:06 +0530

    c operators programm

--- operators.c
+++ operators.c
@@ -14,9 +14,32 @@ int main()
 	printf("a / b = %d\n", a / b); 
 	printf("a % b = %d\n", a % b); 
 	printf("+a = %d\n", +a); 
-	printf("-a = %d\n", -a); 
+	printf("-a = %d\n", -a);    
 	printf("a++ = %d\n", a++); 
 	printf("a-- = %d\n", a--); 
 
 	return 0; 
 }
+
+//2
+// C program to illustrate the relational operators 
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int a = 25, b = 5; 
+
+	// using operators and printing results 
+	printf("a < b : %d\n", a < b); 
+	printf("a > b : %d\n", a > b); 
+	printf("a <= b: %d\n", a <= b); 
+	printf("a >= b: %d\n", a >= b); 
+	printf("a == b: %d\n", a == b); 
+	printf("a != b : %d\n", a != b); 
+
+	return 0; 
+}
+
+
+//3
