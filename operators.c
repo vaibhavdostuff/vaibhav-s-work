48fcb3d002247548938c49acb42cef79d425054e
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-20 20:16:18 +0530

    c operators programm

--- operators.c
+++ operators.c
@@ -62,3 +62,27 @@ int main()
 	return 0; 
 }
 
+//4
+// C program to illustrate the arithmatic operators 
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int a = 25, b = 5; 
+
+	// using operators and printing results 
+	printf("a = b: %d\n", a = b); 
+	printf("a += b: %d\n", a += b); 
+	printf("a -= b: %d\n", a -= b); 
+	printf("a *= b: %d\n", a *= b); 
+	printf("a /= b: %d\n", a /= b); 
+	printf("a %= b: %d\n", a %= b); 
+	printf("a &= b: %d\n", a &= b); 
+	printf("a |= b: %d\n)", a |= b); 
+	printf("a >>= b: %d\n", a >> b); 
+	printf("a <<= b: %d\n", a << b); 
+
+	return 0; 
+}
+
