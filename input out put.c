6339c7d1f3c737fcdcd944b8e97a02745314d5d8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-20 20:23:24 +0530

    c input output programm

--- input out put.c
+++ input out put.c
@@ -1,3 +1,4 @@
+//1
 // C program to show input and output
 
 #include <stdio.h>
@@ -39,6 +40,41 @@ int main()
 
 	// Output the Character
 	printf("\nEntered character is: %c", ch);
+
+	return 0;
+}
+
+//2
+// C program to show input and output
+
+#include <stdio.h>
+
+int main()
+{
+
+	// Declare string variable
+	// as character array
+	char str[50];
+
+	// --- String ---
+	// To read a word
+
+	// Input the Word
+	printf("Enter the Word: ");
+	scanf("%s\n", str);
+
+	// Output the Word
+	printf("\nEntered Word is: %s", str);
+
+	// --- String ---
+	// To read a Sentence
+
+	// Input the Sentence
+	printf("\n\nEnter the Sentence: ");
+	scanf("%[^\n]\ns", str);
+
+	// Output the String
+	printf("\nEntered Sentence is: %s", str);
 
 	return 0;
 }
