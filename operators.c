ca91e49c7fe32945d50db1d021a89081431ee4ac
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-20 20:19:28 +0530

    c operators programm

--- operators.c
+++ operators.c
@@ -86,3 +86,39 @@ int main()
 	return 0; 
 }
 
+//5
+// C Program to demonstrate the use of Misc operators 
+#include <stdio.h> 
+
+int main() 
+{ 
+	// integer variable 
+	int num = 10; 
+	int * add_of_num = #
+
+	printf("sizeof(num) = %d bytes\n", sizeof(num)); 
+	printf("&num = %p\n", &num); 
+	printf("*add_of_num = %d\n", *add_of_num); 
+	printf("(10 < 5) ? 10 : 20 = %d\n", (10 < 5) ? 10 : 20); 
+	printf("(float)num = %f\n", (float)num); 
+
+	return 0; 
+}
+
+//6
+#include <stdio.h> 
+
+int main() 
+{ 
+    // integer variable 
+    int num = 10; 
+    int *add_of_num = &num; // Assign the address of num to add_of_num
+
+    printf("sizeof(num) = %lu bytes\n", sizeof(num)); // Use %lu for size_t
+    printf("&num = %p\n", (void *)&num); // Cast to void pointer
+    printf("*add_of_num = %d\n", *add_of_num); // Dereference the pointer
+    printf("(10 < 5) ? 10 : 20 = %d\n", (10 < 5) ? 10 : 20); 
+    printf("(float)num = %f\n", (float)num); // Cast num to float
+
+    return 0;
+}
