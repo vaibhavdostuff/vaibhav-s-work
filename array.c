606d643a6013c405a13d8070efe3eb5ec6f21bc1
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-21 17:13:22 +0530

    c array programm

--- array.c
+++ array.c
@@ -57,3 +57,48 @@ int main()
 
 	return 0;
 }
+
+//4
+// C Program to demonstrate the use of array
+#include <stdio.h>
+
+int main()
+{
+	// array declaration and initialization
+	int arr[5] = { 10, 20, 30, 40, 50 };
+
+	// modifying element at index 2
+	arr[2] = 100;
+
+	// traversing array using for loop
+printf("Elements in Array: ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
+
+//5
+// C Program to illustrate the use of 1D array
+#include <stdio.h>
+
+int main()
+{
+
+	// 1d array declaration
+	int arr[5];
+
+	// 1d array initialization using for loop
+	for (int i = 0; i < 5; i++) {
+		arr[i] = i * i - 2 * i + 1;
+	}
+
+	printf("Elements of Array: ");
+	// printing 1d array by traversing using for loop
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
