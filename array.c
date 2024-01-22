d1ca748091639d02cb39ee9e8b7b963e0128ef35
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-22 21:57:29 +0530

    c array programm

--- array.c
+++ array.c
@@ -239,3 +239,20 @@ int main()
 	}
 	return 0;
 }
+
+//12
+// This C program compiles fine
+// as index out of bound
+// is not checked in C.
+
+#include <stdio.h>
+
+int main()
+{
+	int arr[2];
+
+	printf("%d ", arr[3]);
+	printf("%d ", arr[-2]);
+
+	return 0;
+}
