b638d54b62e2b0266b1d352137eb781e840a7eed
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-22 21:59:22 +0530

    c array programm

--- array.c
+++ array.c
@@ -256,3 +256,15 @@ int main()
 
 	return 0;
 }
+
+//13
+#include <stdio.h>
+int main()
+{
+
+	// Array declaration by initializing it 
+	// with more elements than specified size.
+	int arr[2] = { 10, 20, 30, 40, 50 };
+
+	return 0;
+}
