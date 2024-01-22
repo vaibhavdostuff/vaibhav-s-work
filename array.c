143a279561935a418bffb647102885d4524732d3
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-22 21:56:36 +0530

    c array programm

--- array.c
+++ array.c
@@ -190,3 +190,27 @@ int main()
 	}
 	return 0;
 }
+
+//10
+// C Program to pass an array to a function
+#include <stdio.h>
+
+void printArray(int arr[])
+{
+	printf("Size of Array in Functions: %d\n", sizeof(arr));
+	printf("Array Elements: ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d ",arr[i]);
+	}
+}
+
+// driver code
+int main()
+{
+
+	int arr[5] = { 10, 20, 30, 40, 50 };
+
+	printf("Size of Array in main(): %d\n", sizeof(arr));
+	printArray(arr);
+	return 0;
+}
