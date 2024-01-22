7a7582d59ae93c4d8e0d756f86f7db75e6750110
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-22 21:56:59 +0530

    c array programm

--- array.c
+++ array.c
@@ -214,3 +214,28 @@ int main()
 	printArray(arr);
 	return 0;
 }
+
+//11
+// C Program to return array from a function
+#include <stdio.h>
+
+// function
+int* func()
+{
+	static int arr[5] = { 1, 2, 3, 4, 5 };
+
+	return arr;
+}
+
+// driver code
+int main()
+{
+
+	int* ptr = func();
+
+	printf("Array Elements: ");
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", *ptr++);
+	}
+	return 0;
+}
