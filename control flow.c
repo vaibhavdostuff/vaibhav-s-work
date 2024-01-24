f4e09559bf497161cc3b32fe112fcbaab6994dc8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-24 23:44:27 +0530

    c control flow programm

--- control flow.c
+++ control flow.c
@@ -74,3 +74,28 @@ int main()
 	else
 		printf("i is not present"); 
 }
+
+//5
+// C Program to illustrate the use of switch statement 
+#include <stdio.h> 
+
+int main() 
+{ 
+	// variable to be used in switch statement 
+	int var = 2; 
+
+	// declaring switch cases 
+	switch (var) { 
+	case 1: 
+		printf("Case 1 is executed"); 
+		break; 
+	case 2: 
+		printf("Case 2 is executed"); 
+		break; 
+	default: 
+		printf("Default Case is executed"); 
+		break; 
+	} 
+
+	return 0; 
+}
