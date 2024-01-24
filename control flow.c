5b4b65b76a6f2285241c0ccf396863bcc6fc0bc8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-24 23:43:15 +0530

    c control flow programm

--- control flow.c
+++ control flow.c
@@ -12,3 +12,22 @@ int main()
 
 	printf("I am Not in if"); 
 }
+
+//2
+// C program to illustrate If statement 
+#include <stdio.h> 
+
+int main() 
+{ 
+	int i = 20; 
+
+	if (i < 15) { 
+
+		printf("i is smaller than 15"); 
+	} 
+	else { 
+
+		printf("i is greater than 15"); 
+	} 
+	return 0; 
+}
