fbe11bcf174ee7a82eb813cc26cf912c833390c7
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-24 23:44:48 +0530

    c control flow programm

--- control flow.c
+++ control flow.c
@@ -99,3 +99,28 @@ int main()
 
 	return 0; 
 }
+
+//7
+// C Program to illustrate the use of conditional operator 
+#include <stdio.h> 
+
+// driver code 
+int main() 
+{ 
+
+	int var; 
+	int flag = 0; 
+
+	// using conditional operator to assign the value to var 
+	// according to the value of flag 
+	var = flag == 0 ? 25 : -25; 
+	printf("Value of var when flag is 0: %d\n", var); 
+
+	// changing the value of flag 
+	flag = 1; 
+	// again assigning the value to var using same statement 
+	var = flag == 0 ? 25 : -25; 
+	printf("Value of var when flag is NOT 0: %d", var); 
+
+	return 0; 
+}
