2c5ed09a044d16046da77d8ddf6d084f0dcb501f
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-25 18:37:01 +0530

    c control flow programm

--- control flow.c
+++ control flow.c
@@ -182,3 +182,19 @@ int main()
 
 	return 0; 
 }
+
+//10
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int gfg = 0; // local variable for main 
+	printf("Before if-else block %d\n", gfg); 
+	if (1) { 
+		int gfg = 100; // new local variable of if block 
+		printf("if block %d\n", gfg); 
+	} 
+	printf("After if block %d", gfg); 
+	return 0; 
+}
