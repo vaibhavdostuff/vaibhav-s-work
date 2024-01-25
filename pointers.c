2669db38e16afdda159651b199bd23e4e30d4641
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-25 18:40:55 +0530

    c pointer  programm

--- pointers.c
+++ pointers.c
@@ -65,3 +65,34 @@ int main()
 	return 0;
 }
 
+//3
+// C program to illustrate Pointer Arithmetic
+
+#include <stdio.h>
+
+int main()
+{
+
+	// Declare an array
+	int v[3] = { 10, 100, 200 };
+
+	// Declare pointer variable
+	int* ptr;
+
+	// Assign the address of v[0] to ptr
+	ptr = v;
+
+	for (int i = 0; i < 3; i++) {
+
+		// print value at address which is stored in ptr
+		printf("Value of *ptr = %d\n", *ptr);
+
+		// print value of ptr
+		printf("Value of ptr = %p\n\n", ptr);
+
+		// Increment pointer ptr by 1
+		ptr++;
+	}
+	return 0;
+}
+
