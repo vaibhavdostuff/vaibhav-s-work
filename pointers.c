53fa38253a9bdc6f98a1a900d669601e63639e3e
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-25 18:42:02 +0530

    c pointer  programm

--- pointers.c
+++ pointers.c
@@ -125,3 +125,23 @@ int main()
 	geeks();
 	return 0;
 }
+
+//4
+// C Program to access array elements using pointers
+#include <stdio.h>
+
+int main()
+{
+
+	// defining array
+	int arr[5] = { 1, 2, 3, 4, 5 };
+
+	// defining the pointer to array
+	int* ptr_arr = arr;
+
+	// traversing array using pointer arithmetic
+	for (int i = 0; i < 5; i++) {
+		printf("%d ", *ptr_arr++);
+	}
+	return 0;
+}
