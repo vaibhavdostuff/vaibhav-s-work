7e076a642426e3ed53b917a84d0ff01af350074a
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-27 22:47:34 +0530

    c functions  programm

--- functions.c
+++ functions.c
@@ -112,3 +112,54 @@ int main()
 		printf("%d is not a prime number", num); 
 	return 0; 
 }
+
+//4
+// C Program to demonstrate Prime Numbers
+// Between Two Intervals Using for 
+// loop in a function
+#include <stdio.h>
+
+// User-defined function to check 
+// prime number
+int checkPrimeNumber(int number)
+{
+	int i, f = 1;
+
+	// Condition for finding the
+	// prime numbers between the
+	// given intervals
+	for (i = 2; i <= number / 2; ++i) 
+	{
+		if (number % i == 0) 
+		{
+			f = 0;
+			break;
+		}
+	}
+
+	return f;
+}
+
+// Driver code
+int main()
+{
+	int num1 = 2, num2 = 10, j, f;
+
+	printf("Prime numbers between %d and %d are: ", 
+			num1, num2);
+	for (j = num1; j < num2; ++j) 
+	{
+		// if flag is equal to 1 then
+		// it is a prime number
+		// calling the function
+		f = checkPrimeNumber(j);
+
+		if (f == 1) 
+		{		 
+			// Printing the result
+			printf("%d ", j);
+		}
+	}
+
+	return 0;
+}
