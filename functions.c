9c1c33bbce795bc11a3fd84ce799aef03dd2a7fe
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-27 22:50:51 +0530

    c functions  programm

--- functions.c
+++ functions.c
@@ -286,3 +286,28 @@ int main()
 	printf("Sum = %d ", recSum(n));
 	return 0;
 }
+
+//8
+// C program to implement the above approach
+#include <stdio.h>
+
+// Function to find factorial of given number
+unsigned int factorial(unsigned int n)
+{
+	int result = 1, i;
+
+	// loop from 2 to n to get the factorial
+	for (i = 2; i <= n; i++) {
+		result *= i;
+	}
+
+	return result;
+}
+
+// Driver code
+int main()
+{
+	int num = 5;
+	printf("Factorial of %d is %d", num, factorial(num));
+	return 0;
+}
