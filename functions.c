da9ac1f0f93cb45db86dc5ea2e0654b9c101b8aa
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-28 20:32:55 +0530

    c functions  programm

--- functions.c
+++ functions.c
@@ -481,3 +481,29 @@ int main()
 	printFib(n); 
 	return 0; 
 }
+
+//13
+// C program to find LCM of 
+// two numbers 
+#include <stdio.h> 
+
+// Driver code 
+int main() 
+{ 
+	int x = 15, y = 25, max; 
+	max = (x > y) ? x : y; 
+
+	// While loop to check if max variable 
+	// is divisible by x and y 
+	while (1) { 
+		if (max % x == 0 && max % y == 0) { 
+			printf("The LCM of %d and %d is %d.", x, y, 
+				max); 
+			break; 
+		} 
+
+		++max; 
+	} 
+
+	return 0; 
+}
