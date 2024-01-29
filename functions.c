b1188c6e55774fc0c45af6735395f4584bd51bfb
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-29 22:15:41 +0530

    c functions  programm

--- functions.c
+++ functions.c
@@ -648,3 +648,33 @@ int main()
 
 	return 0; 
 }
+
+//19
+// C program to find GCD of two numbers 
+#include <math.h> 
+#include <stdio.h> 
+
+// Function to return gcd of a and b 
+int gcd(int a, int b) 
+{ 
+	// Find Minimum of a and b 
+	int result = ((a < b) ? a : b); 
+	while (result > 0) { 
+		// Check if both a and b are divisible by result 
+		if (a % result == 0 && b % result == 0) { 
+			break; 
+		} 
+		result--; 
+	} 
+	// return gcd of a nd b 
+	return result; 
+} 
+
+// Driver program to test above function 
+int main() 
+{ 
+	int a = 98, b = 56; 
+	printf("GCD of %d and %d is %d ", a, b, gcd(a, b)); 
+	return 0; 
+} 
+// This code is contributed by Suruchi Kumari
