92954c1a40bd2722262f7a6dd897184dcdecc9a3
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-30 20:17:04 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -119,3 +119,19 @@ int main()
 	printf("the string is: %s\n", str); 
 	return 0; 
 }
+
+//5
+// C program to demonstrate working of strol()
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	// To store Number in String form.
+	char string[10] = "1234567890";
+	long integer;
+
+	// Base is 10 because we are converting to integer.
+	integer = strtol(string, NULL, 10);
+	printf("Number is %lu", integer);
+}
