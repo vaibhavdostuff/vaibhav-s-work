a7013af43b9c4ff9d1a9aa3db73dd82821d2e491
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-30 19:56:37 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -15,3 +15,18 @@ int main()
 	}
 	return 0;
 }
+
+//2
+// C program to demonstrate Boolean to String
+// Conversion using ternary operator
+#include <stdbool.h>
+#include <stdio.h>
+int main()
+{
+
+	bool n = true;
+
+	const char* s = (n == true) ? "true" : "false";
+	printf("%s", s);
+	return 0;
+}
