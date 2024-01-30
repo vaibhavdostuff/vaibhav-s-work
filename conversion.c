db359e313481a95bb0a792bcf9cc34ea27765357
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-30 20:17:17 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -135,3 +135,23 @@ int main()
 	integer = strtol(string, NULL, 10);
 	printf("Number is %lu", integer);
 }
+
+//6
+// C program to demonstrate working of strol()
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	char string[40] = "100 GeeksforGeeks";
+	long integer;
+	char* ptr;
+
+	// strtol function to convert number in string form to
+	// long integer with base 10
+	integer = strtol(string, &ptr, 10);
+	printf("Integer part is %lu\n", integer);
+	printf("String part is %s\n", ptr);
+
+	return 0;
+}
