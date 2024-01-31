fb7d376e56c2e65c0d02511199d487c6ea9efd6e
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-31 21:03:50 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -284,3 +284,17 @@ int main()
 	printf("%c", (char)(N));
 	return 0;
 }
+
+//15
+// C program to demonstrate conversion of
+// int to char using typecasting
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	int N = 71;
+	char c = N;
+	printf("%c", c);
+	return 0;
+}
