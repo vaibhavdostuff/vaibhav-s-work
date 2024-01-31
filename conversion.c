6da2aafa281334067075d951b73a8e840229748b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-31 21:04:05 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -298,3 +298,18 @@ int main()
 	printf("%c", c);
 	return 0;
 }
+
+//16
+// C program to demonstrate conversion of
+// int to char using sprintf()
+#include <stdio.h>
+int main()
+{
+	int num = 71;
+	char c[1];
+
+	sprintf(c, "%c", num);
+	printf("The character value is %s", c);
+
+	return 0;
+}
