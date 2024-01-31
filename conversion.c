896725a6e9e79f22cf892afc14495ed3366dc4dc
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-31 21:03:30 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -270,3 +270,17 @@ int main()
 	printf("The string is : %s", str); 
 	return 0; 
 }
+
+//14
+// C program to demonstrate
+// conversion of int to char
+// using typecasting
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	int N = 103;
+	printf("%c", (char)(N));
+	return 0;
+}
