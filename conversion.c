952fe7604e76c56ed2811c8f1094d0e784731aae
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-31 21:05:14 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -325,3 +325,17 @@ int main()
 	printf("%c", c);
 	return 0;
 }
+
+//18
+#include <stdio.h>
+
+int main() 
+{
+	int num=71;
+	if (num >= 0 && num <= 127) {
+		printf("The character representation of %d is: %c\n", num, num);
+	} else {
+		printf("The integer %d is out of range for character representation\n", num);
+	}
+	return 0;
+}
