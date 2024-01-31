eb50be3b1f2f4a1e810fdaeb1d294f5ea9c55a25
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-31 21:01:57 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -229,3 +229,15 @@ int main() {
 	printf("%ld\n", result);
 	return 0;
 }
+
+//11
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char input[100] = "123456789";
+	long num;
+	sscanf(input, "%ld", &num);
+	printf("%ld\n", num);
+	return 0;
+}
