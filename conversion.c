3fafa23b054fde1d9409649365e8f76a4fec08c8
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-31 21:01:43 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -205,3 +205,27 @@ int main()
 	// Printing the number
 	printf("Number is %lu", integer);
 }
+
+//10
+#include <stdio.h>
+#include <string.h>
+
+long stringToLong(char *str) {
+	long result = 0;
+	int sign = 1, i = 0;
+	if (str[0] == '-') {
+		sign = -1;
+		i++;
+	}
+	for (; str[i] != '\0'; i++) {
+		result = result * 10 + str[i] - '0';
+	}
+	return sign * result;
+}
+
+int main() {
+	char str[] = "123456789";
+	long result = stringToLong(str);
+	printf("%ld\n", result);
+	return 0;
+}
