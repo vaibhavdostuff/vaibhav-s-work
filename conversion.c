8b631d3aa3946793004d0aa1b370ab8a3489f947
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-01-31 21:02:33 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -241,3 +241,18 @@ int main() {
 	printf("%ld\n", num);
 	return 0;
 }
+
+//12
+// C Program for Long to 
+// String Conversion 
+#include <stdio.h> 
+#include <string.h> 
+
+#define Max_Digits 10 
+int main() 
+{ 
+	long N = 1243; 
+	char str[Max_Digits + sizeof(char)]; 
+	sprintf(str, "%ld", N); 
+	printf("string is: %s \n", str); 
+}
