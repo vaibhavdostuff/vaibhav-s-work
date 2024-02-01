37ae5bc56b83a13969b0645c4d31269881766d22
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-01 21:31:42 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -339,3 +339,20 @@ int main()
 	}
 	return 0;
 }
+
+//19
+// C program to demonstrate conversion of
+// octal to decimal using format specifier
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	int n;
+	printf("Enter an Octal number: \n");
+	scanf("%o", &n);
+
+	// printing the result
+	printf("\nDecimal Representation is: %d", n);
+	return 0;
+}
