659e1bba886d6ceb289300b9303704d0c5a6bd28
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-01 21:34:37 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -535,3 +535,21 @@ int main()
 	decimaltoOctal(n); 
 	return 0; 
 }
+
+//25
+// C program to demonstrate hexadecimal to decimal 
+// conversion using format specifier 
+
+#include <stdio.h> 
+
+int main() 
+{ 
+
+	int n; 
+	// taking input from user 
+	scanf("%x", &n); 
+	
+	// printing the decimal number 
+	printf("%d", n); 
+	return 0; 
+}
