b642ab962738f3aa28c9e137c796be56db1792ff
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-01 21:32:41 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -398,3 +398,37 @@ int main()
 	printf("decimal number is %d",
 		octalToDecimal(octalnum));
 }
+
+//21
+// C program to demonstrate conversion of
+// octal to decimal using pow()
+#include <math.h>
+#include <stdio.h>
+
+int Octaltodecimal(int octal)
+{
+	int decimalnumber = 0, i = 0;
+
+	// while loop executes the statements 
+	// until the condition is false
+	while (octal != 0) 
+	{
+		// Calculating equivalent decimal number 
+		// for the given octal number
+		decimalnumber = decimalnumber + 
+						(octal % 10) * pow(8, i++);
+		octal = octal / 10;
+	}
+	// printing the result
+	return decimalnumber;
+}
+// Driver code
+int main()
+{
+	int octalnumber = 123;
+
+	// calling function
+	printf("decimal number is %d",
+		Octaltodecimal(octalnumber));
+	return 0;
+}
