f6ad70eb2e76f52c886d4bc9de50c88ef653c64c
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-01 21:33:00 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -432,3 +432,31 @@ int main()
 		Octaltodecimal(octalnumber));
 	return 0;
 }
+
+//21
+// C program to demonstrate conversion of
+// octal to decimal without using functions
+#include <math.h>
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	int octalnumber = 123, decimalnumber = 0;
+	int i = 0;
+
+	// while loop executes the statements 
+	// until the condition is false
+	while (octalnumber != 0) 
+	{	 
+		// Calculating equivalent decimal number 
+		// for the given octal number
+		decimalnumber = decimalnumber
+						+ (octalnumber % 10) * pow(8, i++);
+		octalnumber = octalnumber / 10;
+	}
+
+	// Printing the result
+	printf("decimal number is: %d", decimalnumber);
+	return 0;
+}
