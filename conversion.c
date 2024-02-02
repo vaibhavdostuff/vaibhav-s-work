04b7fd1318022653e3d308476129dd29d4fd595b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-02 23:26:38 +0530

    c conversion programm

--- conversion.c
+++ conversion.c
@@ -742,3 +742,18 @@ int main() {
 	printf("Decimal number = %lld", decimalnumber); 
 	return 0; 
 }
+
+//30
+// C Program to demonstrate Decimal to Hexadecimal 
+// Conversion using the format specifier 
+#include <stdio.h> 
+
+int main() 
+{ 
+	int decimalNumber = 45; 
+	
+	// printing hexadecimal number 
+	// using format specifier %X 
+	printf("Hexadecimal number is: %X", decimalNumber); 
+	return 0; 
+}
