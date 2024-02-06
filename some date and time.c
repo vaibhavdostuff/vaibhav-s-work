b5b82a585f87ef16b4ae36e95bbe3c0312c95f5d
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-06 18:27:07 +0530

    c Date and Time  programm

--- some date and time.c
+++ some date and time.c
@@ -185,3 +185,27 @@ char* getMonthName(int monthNumber)
     return month;  
 }  
   
+//5
+// C program to demonstrate getdate() method
+
+#include <dos.h>
+#include <stdio.h>
+
+int main()
+{
+	struct date dt;
+
+	// This function is used to get
+	// system's current date
+	getdate(&dt);
+
+	printf("System's current date\n");
+	printf("%d/%d/%d",
+		dt.da_day,
+		dt.da_mon,
+		dt.da_year);
+
+	return 0;
+}
+
+//6
