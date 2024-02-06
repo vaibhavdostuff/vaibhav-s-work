96166f75a731663e825fe648621acbd4270b81de
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-06 18:29:03 +0530

    c Date and Time  programm

--- some date and time.c
+++ some date and time.c
@@ -303,3 +303,38 @@ int main()
 } 
 
 // This code is contributed by Tapesh (tapeshdua420)
+
+//8
+// C program to illustrate the gmtime() function
+#include <stdio.h>
+#include <time.h>
+#define CST (+8)
+#define IND (-5)
+
+int main()
+{
+
+	// object
+	time_t current_time;
+
+	// pointer
+	struct tm* ptime;
+
+	// use time function
+	time(¤t_time);
+
+	// gets the current-time
+	ptime = gmtime(¤t_time);
+
+	// print the current time
+	printf("Current time:\n");
+
+	printf("Beijing ( China ):%2d:%02d:%02d\n",
+		(ptime->tm_hour + CST) % 24, ptime->tm_min,
+		ptime->tm_sec);
+
+	printf("Delhi ( India ):%2d:%02d:%02d\n",
+		(ptime->tm_hour + IND) % 24, ptime->tm_min,
+		ptime->tm_sec);
+	return 0;
+}
