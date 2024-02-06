3085affe672d8a0542bc6d3ef21e02c83a0513d6
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-06 18:27:28 +0530

    c Date and Time  programm

--- some date and time.c
+++ some date and time.c
@@ -209,3 +209,34 @@ int main()
 }
 
 //6
+// C program to demonstrate setdate() method
+
+#include <dos.h>
+#include <stdio.h>
+
+int main()
+{
+	struct date dt;
+
+	// This function is used to get
+	// system's current date
+	getdate(&dt);
+
+	printf("System's current date\n");
+	printf("%d/%d/%d",
+		dt.da_day,
+		dt.da_mon,
+		dt.da_year);
+
+	printf("Enter date in the format (date month year)\n");
+	scanf("%d%d%d", &dt.da_day, &dt.da_mon, &dt.da_year);
+
+	// This function is used to change
+	// system's current date
+	setdate(&dt);
+
+	printf("System's new date (dd/mm/yyyy)\n")
+		printf("%d%d%d", dt.da_day, dt.da_mon, dt.da_year);
+
+	return 0;
+}
