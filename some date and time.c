cf488b1159d9ff88e840eec2b0f433736b9d78dc
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-06 18:30:11 +0530

    c Date and Time  programm

--- some date and time.c
+++ some date and time.c
@@ -368,3 +368,30 @@ int main()
 		(ptime->tm_hour + ART) % 24, ptime->tm_min, ptime->tm_sec); 
 	return 0; 
 } 
+
+//10
+// C program to convert hours 
+// into minutes and seconds 
+#include <stdio.h> 
+
+// Function to convert hours 
+// into minutes and seconds 
+void ConvertHours(int n) 
+{ 
+	long long int minutes, seconds; 
+
+	minutes = n * 60; 
+	seconds = n * 3600; 
+	
+	printf("Minutes = %lld, Seconds = %lld\n",minutes,seconds); 
+} 
+
+// Driver code 
+int main() 
+{ 
+	int n = 5; 
+	ConvertHours(n); 
+	return 0; 
+} 
+
+// This code is contributed by kothavasaakash.
