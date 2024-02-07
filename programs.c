09fcda7913a50e2737ef1b1726060371708a0dd7
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-07 22:04:57 +0530

    c programm

--- programs.c
+++ programs.c
@@ -57,3 +57,17 @@ int main()
 	scanf("%d", &GFG);
 	return 0;
 }
+
+//6
+// C program to demonstrate
+// a syntax error due to 
+// missing semi colon
+#include <stdio.h>
+
+// Driver code
+int main() 
+{
+// missing semicolon
+printf("Geeks for geeks!") 
+return 0;
+}
