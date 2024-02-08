7aadcd9a1bb858d5ed3977d0df364ad64c4945c1
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-08 17:46:28 +0530

    c programm

--- programs.c
+++ programs.c
@@ -97,3 +97,20 @@ int array[5];
 printf("%d", array[10]);
 return 0;
 }
+
+//9
+// C program to demonstrate
+// a logical error
+#include <stdio.h>
+
+// Driver code
+int main() 
+{
+int i; 
+for(i = 0; i <= 5; i++); 
+{
+	printf("Geeks for Geeks");
+}
+	return 0;
+}
+
