e99624b61a6421a727f180fb7deb439078a5e6aa
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-08 17:48:54 +0530

    c programm

--- programs.c
+++ programs.c
@@ -141,3 +141,15 @@ printf("%d", c);
 	return 0;
 }
 
+//12
+// C program to demonstrate 
+// unreachable code error 
+#include <stdio.h> 
+
+// Driver code 
+int main() 
+{ 
+	printf("geeks for geeks"); 
+	return 0; 
+	printf("gfg"); 
+} 
