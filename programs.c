a7d23f0c69bcdaa50ceb45c4a172f533cc1fa32b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-09 23:46:56 +0530

    c programm

--- programs.c
+++ programs.c
@@ -415,3 +415,27 @@ int main()
 // Prints location of C this C code.
 printf("%s", __FILE__);
 } 
+
+//23
+// C program that prints its source code.
+#include<stdio.h>
+
+// Driver code
+int main(void)
+{
+	// We can append this code to any C program
+	// such that it prints its source code.
+	char c; 
+	FILE *fp = fopen(__FILE__, "r");
+
+	do
+	{
+		c = fgetc(fp);
+		putchar(c);
+	}
+	while (c != EOF);
+
+	fclose(fp);
+
+	return 0;
+}
