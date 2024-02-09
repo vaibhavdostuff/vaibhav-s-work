d9ce1894a9bbdc23ba249a804f5972e68fe2ef0b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-09 23:41:28 +0530

    c programm

--- programs.c
+++ programs.c
@@ -236,3 +236,32 @@ if (N == 4)
 } 
 return 0; 
 }
+
+//17
+// C program to find quotient
+// and remainder of two numbers
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	int A, B, quotient = 0, remainder = 0;
+
+	// Ask user to enter the two numbers
+	printf("Enter two numbers A and B : \n");
+
+	// Read two numbers from the user || A = 17, B = 5
+	scanf("%d%d", &A, &B);
+
+	// Calculate the quotient of A and B using '/' operator
+	quotient = A / B;
+
+	// Calculate the remainder of A and B using '%' operator
+	remainder = A % B;
+
+	// Print the result
+	printf("Quotient when A/B is: %d\n", quotient);
+	printf("Remainder when A/B is: %d", remainder);
+
+	return 0;
+}
