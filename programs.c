a322153425d7b8c311d9aa09444c7be95fd1ee18
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-09 23:41:49 +0530

    c programm

--- programs.c
+++ programs.c
@@ -265,3 +265,30 @@ int main()
 
 	return 0;
 }
+
+//18
+// C program to find quotient and
+// remainder
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+	int A = 17, B = 5;
+
+	int quotient = 0;
+
+	// Initializing the loop
+	while (quotient * B <= A)
+		quotient++;
+
+	// Decrementing 1 because we came
+	// one step forward
+	quotient--;
+
+	int remainder = A - (B * quotient);
+
+	printf("Quotient when A/B is: %d\n", quotient);
+	printf("Remainder when A/B is: %d", remainder);
+	return 0;
+}
