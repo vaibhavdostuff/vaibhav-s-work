933e0525c522e552d013b957aea53dcc6be8d1d4
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-02-09 23:46:30 +0530

    c programm

--- programs.c
+++ programs.c
@@ -403,3 +403,15 @@ int main()
 
 	return 0; 
 } 
+
+//22
+// C program to display the 
+// location of the file
+#include <stdio.h>
+
+// Driver code
+int main()
+{
+// Prints location of C this C code.
+printf("%s", __FILE__);
+} 
