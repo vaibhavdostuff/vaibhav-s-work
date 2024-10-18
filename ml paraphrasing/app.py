c1eb9ed48d4a93bd9077ccb09520b2fbea17d16b
vaibhavdostuff <vaibhavnegi141@gmail.com>
2024-10-18 18:16:06 +0530

    scraping

--- ml paraphrasing/app.py
+++ ml paraphrasing/app.py
@@ -3,3 +3,10 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, AutoModelForSeque
 import torch
 from textblob import TextBlob  # For auto-correction
 
+app = Flask(__name__)
+
+# Load the paraphrasing model and tokenizer
+paraphrase_model_name = "t5-large"
+paraphrase_tokenizer = AutoTokenizer.from_pretrained(paraphrase_model_name)
+paraphrase_model = AutoModelForSeq2SeqLM.from_pretrained(paraphrase_model_name)
+
